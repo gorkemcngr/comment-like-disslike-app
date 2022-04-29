75b93ee2910142b84b87e8374974430b6f21b1bb
Gorkem <81441348+gorkemcngr@users.noreply.github.com>
2022-04-29 19:17:10 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Comment Like/Dislike App</title>
+  </head>
+  <body>
+    <header>Comment Like/Dislike App</header>
+    <div class="container">
+      <div  class="comment--like--dislike--app">
+            <ul v-for="comment in commentList" :key="comment.id">
+        <appComments :content="comment.content"   />
+        </ul>
+   
+
+  
+      </div>
+    </div>
+  </body>
+</html>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import appComments from "@/components/appComments"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  
+  data(){
+    return {
+      commentList: [{id: 1, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deserunt, tempore accusamus iusto nobis dolore ratione itaqueperferendis delectus? Nostrum corporis, quod voluptates quis consequuntur eveniet beatae dolor aperiam ad.'},
+      {id: 2, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deserunt, tempore accusamus iusto nobis dolore ratione itaqueperferendis delectus? Nostrum corporis, quod voluptates quis consequuntur eveniet beatae dolor aperiam ad.'},
+      {id: 3, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deserunt, tempore accusamus iusto nobis dolore ratione itaqueperferendis delectus? Nostrum corporis, quod voluptates quis consequuntur eveniet beatae dolor aperiam ad.'},
+      {id: 4, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deserunt, tempore accusamus iusto nobis dolore ratione itaqueperferendis delectus? Nostrum corporis, quod voluptates quis consequuntur eveniet beatae dolor aperiam ad.'}],
+      
+    }
+  },
+  components : {
+    appComments
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+@import 'assets/style.css';
 </style>
---
+++ src/components/appComments.vue
@@ -0,0 +1,26 @@
+<template>
+          
+        <div class="card mb-10 comment--item">
+          <p>
+           {{content}}
+          </p>
+          <div class="action--button--container text-right mt-10">
+            <button @click="likeCounter++" class="btn-sm btn-success">Beğendim ({{likeCounter}})</button>
+            <button @click="disslikeCounter++" class="btn-sm btn-danger">Beğenmedim ({{disslikeCounter}})</button>
+          </div>
+        </div>
+        
+
+</template>
+
+<script>
+export default {
+  props: ['content'],
+  data(){
+    return{
+      likeCounter:0,
+      disslikeCounter:0
+    }
+  }
+}
+</script>
